1d45be30599687fc54be5a9441024ef9d164c989
Leif Cordero <124620285+leifcordero14@users.noreply.github.com>
2025-08-27 16:43:58 -0400

    refactor: modularize en.employees.local object

---
+++ src/infrastructure/local/en/employees/en.employees.form.local.ts
@@ -0,0 +1,9 @@
+export const form = {
+  fullName: 'Full Name',
+  email: 'Email',
+  position: 'Position',
+  phone: 'Phone',
+  status: 'Status',
+  update: 'Update',
+  add: 'Add'
+}
---
+++ src/infrastructure/local/en/employees/en.employees.table.local.ts
@@ -0,0 +1,8 @@
+export const table = {
+  employee: 'Employee',
+  position: 'Position',
+  status: 'Status',
+  joinDate: 'Join Date',
+  lastActive: 'Last Active',
+  actions: 'Actions'
+}
